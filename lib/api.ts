30b8c3ab0aa225d9f274a952f649c556061265cf
RehanDias <rehanpratama282@gmail.com>
2025-04-12 20:18:02 +0700

    repair api

--- lib/api.ts
+++ lib/api.ts
@@ -1,9 +1,4 @@
-const CORS_PROXY = 'https://api.allorigins.win/get?url=';
-
 export async function fetchTiktokLiveData(username: string) {
-    const targetUrl = `https://tiktok-live-stream.vercel.app/api/tiktok-live/${username}`;
-    const encodedUrl = encodeURIComponent(targetUrl);
-    const response = await fetch(`${CORS_PROXY}${encodedUrl}`);
-    const data = await response.json();
-    return JSON.parse(data.contents);
+    const response = await fetch(`https://tiktok-live-stream.vercel.app/api/tiktok-live/${username}`);
+    return response.json();
 }
